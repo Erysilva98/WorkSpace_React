39dada8474714dbe4df2542f501bd92b1999fbfb
Erysilva98 <erimilsonsilva98@gmail.com>
2023-03-12 18:05:13 -0300

    base form criada

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,43 @@
 import React from "react";
 import logo from "../img/logo.png";
-import mouraTech from "../img/mouratech.png";
 
 function Login()
 {
     return (
-        <div className="flex flex-col jus-center items-center h-screen">
-            <img src="{logo}" ></img>
-            <form className="w-full max-w-sm">
-                <div className="mb-4">
-                    <label className="block">
-                        Usuário
-                    </label>
+        <>
+            <header>
+                <img src={logo} alt="Logo da Moura"/>
+                <span>Descrição</span>
+            </header>
+
+            <form>
+                <div>
+                    <label htmlFor="email">E-mail: </label>
+                    <input 
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder=" emailPessoal@gmail.com "
+                    />
                 </div>
 
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input 
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder=" *********** "
+                    />
+                </div>
+
+                <a href="/">Esqueceu sua Senha ? </a>
+
+                <div>
+                    <button>Entrar</button>
+                </div>
             </form>
-            
-        </div>
+        </>
     );
 }
 
